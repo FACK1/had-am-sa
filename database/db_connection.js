5267c88d69790e4033b48b54885ea282c3466465
samaamro20 <samaamro20@gmail.com>
2018-12-05 14:37:00 +0200

    change ssl #6

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,11 +1,18 @@
 const {Pool} = require('pg');
+const url = require('url');
 require('env2')('config.env');
-const { DATABASE_URL } = process.env;
+let { DATABASE_URL } = process.env;
+
+if (process.env.NODE_ENV === "test") {
+  DATABASE_URL = process.env.TEST_DB_URL;
+}
 if(!DATABASE_URL){
   throw Error('No Data Base URL');
 }
+
 const option = {
   connectionString : DATABASE_URL
 };
-option.ssl = option.host !== 'localhost';
+const hostname = url.parse(DATABASE_URL).hostname;
+option.ssl =(hostname !== 'localhost');
 module.exports = new Pool(option);
