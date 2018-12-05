cb46e4842d9069daaa88d98012039c73a3649dcc
samaamro20 <samaamro20@gmail.com>
2018-12-05 11:54:54 +0200

    change db varible name #6

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
 require('env2')('config.env');
-const { DB_URL } = process.env;
-if(!DB_URL){
+const { DATABASE_URL } = process.env;
+if(!DATABASE_URL){
   throw Error('No Data Base URL');
 }
 const option = {
-  connectionString : DB_URL
+  connectionString : DATABASE_URL
 };
 module.exports = new Pool(option);
