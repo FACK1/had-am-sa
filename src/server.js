bf80f3cab849d3dd6ebc3ce03c4c21286f62dbe9
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-06 08:37:39 +0200

    change console

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const router = require('./router.js');
 const server = http.createServer(router);
 const PORT = process.env.PORT || 3003;
-server.listen(PORT, function(){
-    console.log(`server is up and running on port ${PORT}!`);
-  });
+server.listen(PORT, function () {
+  console.log(`server is up and running on the port number ${PORT}!`);
+});
