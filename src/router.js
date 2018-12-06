32361dcb6f5354a2d07090a933f41ffdbf3aede5
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-06 08:38:52 +0200

    comment

--- src/router.js
+++ src/router.js
@@ -1,28 +1,29 @@
 const handlers = require('./handler.js');
 
 //------Create Router-----------------------------------------------------------
-const router = (request, response)=> {
+const router = (request, response) => {
 
-//----Home Endpoint-------------------------------------------------------------
+      // new comment
+      //----Home Endpoint-------------------------------------------------------------
       if (request.url === '/') {
-            handlers.homeHandler(request,response)
+            handlers.homeHandler(request, response)
       }
-    //-----Client Side Endpoint-------------------------------------------------
-    else if (request.url.includes('/public/')) {
-            handlers.publicHandler(request,response)
-    }
-    //-----Search Endpoint-----------------------------------------------------
-    else if (request.url.includes('/search')) {
-          handlers.searchHandler(request,response)
-    }
-    //-----add Endpoint-----------------------------------------------------
-    else if (request.url.includes('/add')) {
-          handlers.addHandler(request,response)
-    }
-    //-----Error Endpoint-------------------------------------------------------
-    else {
-          handlers.pageNotFoundHandler(request,response)
-    }
+      //-----Client Side Endpoint-------------------------------------------------
+      else if (request.url.includes('/public/')) {
+            handlers.publicHandler(request, response)
+      }
+      //-----Search Endpoint-----------------------------------------------------
+      else if (request.url.includes('/search')) {
+            handlers.searchHandler(request, response)
+      }
+      //-----add Endpoint-----------------------------------------------------
+      else if (request.url.includes('/add')) {
+            handlers.addHandler(request, response)
+      }
+      //-----Error Endpoint-------------------------------------------------------
+      else {
+            handlers.pageNotFoundHandler(request, response)
+      }
 
-    }
-  module.exports = router
+}
+module.exports = router
